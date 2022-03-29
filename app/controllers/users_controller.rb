3c220cfa180a805852faf065071364f66bb737e0
Kohei Uno <Ukouhei96@gmail.com>
2022-03-29 13:58:57 +0900

    :white_check_mark: add integration test that is user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = 'Welecome to the Sample Application!'
+      redirect_to @user  # redirect_to user_url(@user)
     else
       render 'new'
     end
