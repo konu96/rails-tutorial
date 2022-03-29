8b664e74f2beae7d44eb200965b3c915dcfdfb0a
Kohei Uno <Ukouhei96@gmail.com>
2022-03-29 10:01:47 +0900

    :construction: add new route for creating users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/home'
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
   get 'static_pages/cat'
   get 'static_pages/contact'
 
+  get '/signup', to: 'users#new'
+
   resources :microposts
-  resources :users
 end
