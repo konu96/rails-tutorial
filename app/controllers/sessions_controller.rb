9c5439d01256b58f0e039c3ce538f7de62271687
Kohei Uno <Ukouhei96@gmail.com>
2022-03-31 10:34:13 +0900

    finish 8 chapter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    session = params[:session]
+    user = User.find_by(email: session[:email].downcase)
+
+    if user && user.authenticate(session[:password])
+      log_in user
+
+      redirect_to user  # user_url(user)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'Welecome to the Sample Application!'
+
+      log_in @user
       redirect_to @user  # redirect_to user_url(@user)
     else
       render 'new'
