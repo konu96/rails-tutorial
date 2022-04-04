12629cb7b8618adf03d1995bdc7d85a8b0ea2b4a
Kohei Uno <Ukouhei96@gmail.com>
2022-04-04 15:35:17 +0900

    :construction: temporary commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session[:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 
       redirect_to user  # user_url(user)
     else
